543280951b74030ce7e1384fccf45eaac84ffcad
Mohcka <mikeduels@yahoo.com>
2020-07-03 09:37:30 -0700

    Add interface for contract 🎛

--- src/services/generic-service.ts
+++ src/services/generic-service.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosResponse } from 'axios'
 
 /**
- * @template T s
+ * @template T The data model that's expected from the server
  */
 export interface ApiWebService<T> {
-  getAll(): Promise<T[]>
+  getAll(): Promise<void | T[]>
 }
 
 // TODO: add docs
@@ -12,7 +12,7 @@ export interface ApiWebService<T> {
  * 
  * @template T The data model that's expected from the server
  */
-export default abstract class BaseService<T> {
+export default abstract class BaseService<T> implements ApiWebService<T> {
   protected readonly apiUrl: string = ''
   constructor(apiUrl: string) {
     this.apiUrl = apiUrl
