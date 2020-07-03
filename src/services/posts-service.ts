11960004469ef6ba15512226b5010006e8bae6f2
Mohcka <mikeduels@yahoo.com>
2020-07-03 09:36:54 -0700

    Tidy up code 🧹

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -5,6 +5,5 @@ import { PostModel } from '../models/post-model'
 export default class PostsService extends BaseService<PostModel> {
   constructor() {
     super(`https://localhost:5001/api/posts`)
-
   }
 }
