30e6434346bbb7cf2772b0d845fe2011b54edbd1
Mohcka <mikeduels@yahoo.com>
2020-07-05 20:23:30 -0700

    Updates constructor

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -3,7 +3,7 @@ import BaseService from './generic-service'
 import { PostModel } from '../models/post-model'
 
 export default class PostsService extends BaseService<PostModel> {
-  constructor() {
-    super(`https://localhost:5001/api/posts`)
+  constructor(apiUrl: string) {
+    super(apiUrl)
   }
 }
