ab54e6aa3011262ed2bb1e52ac3a6328a9aef880
Mohcka <mikeduels@yahoo.com>
2020-07-06 17:04:50 -0700

    Updated authentication method 🔒

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { launch } from "./App"
-;import { AxiostHttpClientHelper } from "./utils/axios-interceptors-herlper";
+import { launch } from './App'
+import { AxiostHttpClientHelper } from './utils/axios-interceptors-herlper'
 
-(() => {
+;(() => {
   // Setup http client for handling token authentication
   // before starting application
   AxiostHttpClientHelper.staticInitAxiosTokenHandling()
