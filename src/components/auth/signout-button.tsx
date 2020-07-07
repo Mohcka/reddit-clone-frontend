d62e0786031f351f330536bdfd4633ca96ac47c2
Mohcka <mikeduels@yahoo.com>
2020-07-06 17:27:25 -0700

    Updates signout method 🔒

--- src/components/auth/signout-button.tsx
+++ src/components/auth/signout-button.tsx
@@ -5,29 +5,16 @@ import Button from '@material-ui/core/Button'
 import Swal from 'sweetalert2'
 
 import { AuthContext } from '../context/AuthContext'
+import { TokenLocalStorage } from '../../utils/token-storage'
 
 const SignoutButton = () => {
   const history = useHistory()
   const { setIsAuthenticated, authService } = useContext(AuthContext)
 
   const signout = () => {
-    authService
-      .signout()
-      .then(() => {
-        // Signout out was success
-        setIsAuthenticated(false)
-        history.push('/')
-      })
-      .catch((err) => {
-        // return home
-        history.push('/')
-        // Display error to client
-        Swal.fire({
-          icon: 'error',
-          title: 'An Error has occured',
-          text: `${err}`,
-        })
-      })
+    TokenLocalStorage.clearToken()
+    setIsAuthenticated(false)
+    history.push('/')
   }
 
   return (
