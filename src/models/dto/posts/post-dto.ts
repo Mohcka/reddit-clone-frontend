3466916ac93af0e612f387ed80f559c51fad9db2
Mohcka <mikeduels@yahoo.com>
2020-07-08 08:58:04 -0700

    Renamed from post-dto

--- src/models/dto/posts/post-dto.ts
+++
@@ -1,4 +0,0 @@
-export interface PostDTO {
-  postTitle: string,
-  postContent: string
-}
---
+++ src/models/posts/post-content.ts
@@ -0,0 +1,6 @@
+export interface PostContent {
+  /** Post's title */
+  postTitle: string
+  /** The body of the post */
+  postContent: string
+}
