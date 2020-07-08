21c8886b8401004a4defaf255d556d65e17ee2ec
Mohcka <mikeduels@yahoo.com>
2020-07-07 22:45:04 -0700

    Fixes routing issue

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
+  output: {
+    publicPath: '/',
+  },
   // Ensures dev server works with react-router
   devServer: {
     historyApiFallback: true,
