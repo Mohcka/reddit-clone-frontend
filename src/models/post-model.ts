320d12a924de424dc0a62773725ca41e265feee5
Mohcka <mikeduels@yahoo.com>
2020-07-08 09:09:33 -0700

    Updates model 👤

--- src/models/post-model.ts
+++ src/models/post-model.ts
@@ -1,8 +1,9 @@
-export interface PostModel {
-  /** Post's title */
-  postTitle: string
-  /** The body of the post */
-  postContent: string
+import { PostContent } from './posts/post-content'
+
+export interface PostModel extends PostContent {
+  id: string
+  /** Id of the user who made this post */
+  userId: string
 }
 
 export type PostsModel = Array<PostModel>
