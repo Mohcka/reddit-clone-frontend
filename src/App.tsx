0330ca4ae24905dcc4134c351b726476293a403c
Mohcka <mikeduels@yahoo.com>
2020-07-07 19:08:31 -0700

    Registers user's access on startup

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,35 @@ import ReactDOM from 'react-dom'
 import { Routes } from './routes/Routes'
 import { BrowserRouter as Router, useHistory } from 'react-router-dom'
 import Navbar from './components/sections/Navbar'
-import { AuthContext, jwtAuthService, UserInfo } from './components/context/AuthContext'
+import {
+  AuthContext,
+  jwtAuthService,
+  UserInfo,
+} from './components/context/AuthContext'
 import { TokenLocalStorage } from './utils/token-storage'
 import { AxiostHttpClientHelper } from './utils/axios-interceptors-herlper'
+import { UserInfoResponseDTO } from './models/dto/user-info-response-dto'
 
 const App = () => {
   const history = useHistory()
 
   // Set global state for context
-  const [isAuthenticated, setIsAuthenticated] = useState(TokenLocalStorage.isAuthenticated());
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    TokenLocalStorage.isAuthenticated()
+  )
   const [userInfo, setUserInfo] = useState<UserInfo>({
     userId: null,
-    userName: null
+    userName: null,
   })
-  const globalAuthValue = {isAuthenticated, setIsAuthenticated, userInfo, setUserInfo, authService: jwtAuthService}
+  const globalAuthValue = {
+    isAuthenticated,
+    setIsAuthenticated,
+    userInfo,
+    setUserInfo,
+    authService: jwtAuthService,
+  }
+
+  
 
   // Setup http client for handling token authentication
   // before starting application
@@ -26,14 +41,22 @@ const App = () => {
     history.push('/')
   })
 
-    return (
-      <AuthContext.Provider value={globalAuthValue}>
-        <Router>
-          <Navbar />
-          <Routes />
-        </Router>
-      </AuthContext.Provider>
-    )
+  // TODO: This feels bad, figure out alternative for determining if a user is logged in on app startup
+  // fetch and set user
+  if (isAuthenticated) {
+    jwtAuthService.getUser().then((data: UserInfoResponseDTO) => {
+      setUserInfo(data)
+    })
+  }
+
+  return (
+    <AuthContext.Provider value={globalAuthValue}>
+      <Router>
+        <Navbar />
+        <Routes />
+      </Router>
+    </AuthContext.Provider>
+  )
 }
 
 export const launch = () => {
