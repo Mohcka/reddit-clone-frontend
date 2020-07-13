3b624cf6e6c7fdef646802535554502422b70401
Mohcka <mikeduels@yahoo.com>
2020-07-13 10:35:11 -0700

    Adds service methods 🛎

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,9 +1,35 @@
 import axios from 'axios'
-import BaseService from './generic-service'
+import BaseService, { ApiWebService } from './generic-service'
 import { PostModel } from '../models/post-model'
+import { CommentModel } from '../models/comment-model'
+import { PostWithCommentsResponseDTO } from '../models/posts/post-with-comments-response-dto'
+import { VoteType } from '../models/vote-model'
+import { VoteResponseDTO } from '../models/dto/votes/vote-response-dto'
 
-export default class PostsService extends BaseService<PostModel> {
+export interface IPostsService extends ApiWebService<PostModel> {
+  getPostWithComments(id: string): Promise<PostWithCommentsResponseDTO>
+}
+
+export default class PostsService extends BaseService<PostModel>
+  implements IPostsService {
   constructor(apiUrl: string) {
     super(apiUrl)
+  }
+
+  getPostWithComments(id: string): Promise<PostWithCommentsResponseDTO> {
+    return axios
+      .get<PostWithCommentsResponseDTO>(`${this.apiUrl}/${id}/comments`)
+      .then((resp) => resp.data)
+  }
+
+  /** Send a request to make a vote on a specifed post */
+  VoteOnPost(id: string, voteType: VoteType): Promise<VoteResponseDTO> {
+    // Send the vote type in the params because that's what the api is expecting
+    // here
+    return axios
+      .post<VoteResponseDTO>(`${this.apiUrl}/${id}`, null, {
+        params: { voteType: voteType },
+      })
+      .then((resp) => resp.data)
   }
 }
